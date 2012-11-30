525dff6082357c41dd8298e3ea3e8eb4d45a1874
stefan <dev@2904.cc>
2012-11-30 22:36:58 +0100

    piraten analyse

--- test1.py
+++ test1.py
@@ -3,13 +3,26 @@ import csv
 sprengel = csv.reader(open("./raw/Graz/GRW2012_Sprengelerg.csv", "r"), delimiter=";")
 
 sprengelNr = -1
+sprengelCount = 0
+gueltigSum = 0
+
+piratenProzentSum = 0
+piratenSum = 0
+
+piratenCHECK = float(2.69726951718)
+groesserCount = 0
+kleinerCount = 0
+maxProzent = 0
+maxSprengel = 0
 
 # wkurz;sprengel;ptname;ptlang;listenplatz;gesamt;unguel;gueltig;stimmen;anzspr;aenderung_am;aenderung_durch
 
 for row in sprengel: 
-	if(row[1].isdigit()):
-		if(sprengelNr != row[1]):
-			print ""
+	if row[1].isdigit():
+		if sprengelNr != row[1]:
+			#print ""
+			sprengelCount+=1
+			gueltigSum+=float(row[7])
 		sprengelNr = row[1]
 		partei = row[3]
 		gesamt = row[5]
@@ -17,6 +30,30 @@ for row in sprengel:
 		stimmen = row[8]
 	
 		prozent = float(stimmen)/float(gueltig)*100
-		print sprengelNr, partei, ":", prozent, "bei", stimmen, "von", gesamt, "Stimmen"
+		#print sprengelNr, partei, ":", prozent, "% bei", stimmen, "von", gesamt, "Stimmen"
+
+		if partei == "Piratenpartei Graz":
+			piratenProzentSum+=prozent
+			piratenSum+=float(stimmen)
+
+			if prozent > maxProzent:
+				maxProzent = prozent
+				maxSprengel = sprengelNr
+
+			if prozent > piratenCHECK:
+				print "Sprengel", sprengelNr, "war um", prozent-piratenCHECK, "groesser"
+				groesserCount+=1
+			else:
+				print "Sprengel", sprengelNr, "war um", piratenCHECK-prozent, "kleiner"
+				kleinerCount+=1
+
 	else:
 		print row
+
+print ""
+print "Piraten im Schnitt", piratenProzentSum/sprengelCount, "%"
+print "Piraten in Summe", piratenSum/gueltigSum*100, "% bei", piratenSum, "Stimmen"
+print "Es waren", groesserCount, "Sprengel groesser als die Summe"
+print "Es waren", kleinerCount, "Sprengel kleiner als die Summe"
+print "Rekord waren", maxProzent, "% in Sprengel", maxSprengel
+print ""	
